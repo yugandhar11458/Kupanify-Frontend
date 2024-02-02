c3c4eea239c360f99ae8c689c14fc57a15cf85d9
yugandhar11458 <150141948+yugandhar11458@users.noreply.github.com>
2024-02-02 16:16:38 +0530

    Update ProfilePage.jsx

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import CouponList from "../components/CouponList";
 import Footer from "../components/Footer";
 import CouponUploadForm from "../components/CouponUploadForm";
