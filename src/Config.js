b233a3e68a3c173a43396e9d10c9c4eb6058566a
yugandhar11458 <150141948+yugandhar11458@users.noreply.github.com>
2024-02-06 14:12:18 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
-const backendIpAddress = 'http://51.20.252.212:8000';  // Backend IP address / url
+const backendIpAddress = import.meta.env.VITE_REACT_APP_BACKEND_URL||'http://localhost:8000';  // Backend IP address / url
 
 export default {
   backendIpAddress,
